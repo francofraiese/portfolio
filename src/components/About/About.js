1368cb4cc7c12b1782e4bd996d23164cc2920f44
Franco Fraiese <86889319+francofraiese@users.noreply.github.com>
2022-08-13 18:07:16 -0300

    responsives about y header

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,12 +7,12 @@ const About = () => {
                 <h4>Hi there! I'm</h4   >
                 <h1>Franco Fraiese</h1>
                 <h2>FullStack Developer</h2>
-                <br/>
+                <br className="skip"/>
                 <h4>I like to be in challenges and also </h4>
-                <h4>like to be constantly <span color="#38F677"> learning    </span> new technologies </h4>
-                <h4>and <span color ="#38F677">getting better</span> in which i already know.</h4>
-                <br/>
-                <h4>I’m an <span color="aqua">Argentinian</span> citizen of Mar del Plata.</h4>
+                <h4>like to be constantly <span className="green"> learning    </span> new technologies </h4>
+                <h4>and <span className="green">getting better</span> in which i already know.</h4>
+                <br className="skip"/>
+                <h4>I’m an <span className="aqua">Argentinian</span> citizen of Mar del Plata.</h4>
             </div>
             <img src="./images/FrancoFraiese.png" alt="FrancoFraiese"/>
         </div>
