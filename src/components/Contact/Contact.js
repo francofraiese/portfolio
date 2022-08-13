e7601c57b562c29e8689645a256ec06993899e6c
Franco Fraiese <86889319+francofraiese@users.noreply.github.com>
2022-08-13 20:34:15 -0300

    antes de cambios peligrosos

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,15 +1,21 @@
 import { click } from '@testing-library/user-event/dist/click';
+import { useState } from 'react';
 import './Contact.css'
+import Copied from './modal/copiedModal';
 
 const Contact = () => {
 
+    const [openModal, setOpenModal] = useState(false)
+
     function copyLink(e){
         e.preventDefault()
-        navigator.clipboard.writeText("fraancofraiese@gmail.com").then(() => {
-            /* clipboard successfully set */
-          }, () => {
-            /* clipboard write failed */
-          });
+        navigator.clipboard.writeText("fraancofraiese@gmail.com")
+        setOpenModal(true)
+    }
+
+    function handleClose(e){
+        e.preventDefault()
+        setOpenModal(false)
     }
     
 
@@ -21,9 +27,18 @@ const Contact = () => {
             <div className="github">
                 <a href="https://github.com/francofraiese/" target="_blank"><img src="./images/github.png" alt="Github"/></a>
             </div>
-            <div className="mail">
-                <a onClick={copyLink}><img src="./images/gmail.png" alt="Copy email"/></a>
-            </div>
+            {openModal !== false ? 
+                <div className="mailopen">
+                    <Copied close={handleClose}/>
+                    <a onClick={copyLink}><img src="./images/gmail.png" alt="Copy email"/></a>
+                    <br/>
+                </div>
+                :
+                <div className="mail">
+                    <a onClick={copyLink}><img src="./images/gmail.png" alt="Copy email"/></a>
+                </div>
+                    }
+
         </div>
     )
 }
---
+++ src/components/Contact/modal/copiedModal.js
@@ -0,0 +1,9 @@
+const Copied = () => {
+ return (
+<div >
+    <h2>copiado!</h2>
+</div>
+ )
+}
+
+export default Copied
